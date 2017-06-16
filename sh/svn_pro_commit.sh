e5a264f4cef2e3cbf59073b690d0424515a41f82
qishanqing <18310287801@163.com>
2017-06-16 18:18:17 +0800

    fixed svn_pro_commit

--- sh/svn_pro_commit.sh
+++ sh/svn_pro_commit.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-set -x 
+#set -x 
 
-h5_author=/home/qishanqing/myscript/product-config/svn/author/h5
-h5_project=/home/qishanqing/myscript/product-config/svn/project/h5 
-            
-for u in `cat $h5_author`;do
-        for p in `cat $h5_project`;do 
-                if [[  `$SVNLOOK author $REPOS` =~ "$u" ]];then
-                        if [[  `$SVNLOOK changed $REPOS -t $TXN` =~ "$p" ]];then
-                                echo "$u: 你没有此文件提交权限"
-                                exit 1
-                        fi
-                fi
-        done
-done
+function h5_access() {
+	local h5_author=/home/qishanqing/myscript/product-config/svn/author/h5
+	local h5_project=/home/qishanqing/myscript/product-config/svn/project/h5 
+	for u in `cat $h5_author`;do
+		if [[ `$SVNLOOK author $REPOS` =~ "$u" ]];then
+			return 0
+		fi
+	done
 
-exit 0
+	for p in `cat $h5_project`;do
+		if [[  `$SVNLOOK changed $REPOS -t $TXN` =~ "$p" ]];then
+			echo -e  “你没有权限提交此文件” 1>&2
+			exit 1
+		fi
+	done
+}
+
+h5_access
