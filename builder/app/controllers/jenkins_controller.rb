78ccd0c52f2af018302c7622c823033a3e62bd2f
qishanqing <18310287801@163.com>
2018-01-24 18:01:06 +0800

    skip jenkins token

--- builder/app/controllers/jenkins_controller.rb
+++ builder/app/controllers/jenkins_controller.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 
 $LOAD_PATH << ( Rails.root + 'app/controllers')
 class JenkinsController < ApplicationController
-
+  skip_before_action :verify_authenticity_token, only: [:new]
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
