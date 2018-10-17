a503ff1d05dc39497576fe1fef5efb1042786987
qishanqing <qishanqing@dafy.com>
2018-10-17 16:49:26 +0800

    upload android apk to ftp

--- sh/jenkins-upload-android.sh
+++ sh/jenkins-upload-android.sh
@@ -7,12 +7,13 @@ set +x
 
 export BUILD_ID=dontkillme
 
-DT=`date '+%Y%m%dT%H%M%S'`
+DT=`date '+%Y%m%d'`
 branch=$SVN_URL
 version=$SVN_REVISION
 job_name=$BUILD_URL
 owner=$BUILD_USER_ID
 jb=$JOB_NAME
+
 
 if [ -f /mnt/svn/task_id.log ];then
     task_id=`cat /mnt/svn/task_id.log || true`
@@ -20,4 +21,17 @@ if [ -f /mnt/svn/task_id.log ];then
     export task_id
 fi
 
-createtag
+function upload_android_version () {
+    local path=/mnt/svn/ANDROID/$DT
+    mkdir -p $path || true
+    
+    find -name *.apk -print | while read filename;do
+	file=$(basename $filename)
+	file=${file%.*}
+	file=$file_$version
+	createtag
+	cp $filename $path/$file.apk && echo "上传成功"
+    done
+}
+
+upload_android_version
