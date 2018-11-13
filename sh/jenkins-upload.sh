ebc3a6770631ea0506b7d861db56e9c2aa18fd18
qishanqing <qishanqing@dafy.com>
2018-11-13 19:16:07 +0800

    other servicebus.xml info check

--- sh/jenkins-upload.sh
+++ sh/jenkins-upload.sh
@@ -25,11 +25,22 @@ function replace_files () {
     fils_name="upload/$file/WEB-INF/classes/servicebus.xml"
     if [ -f "$fils_name" ];then
 	p=$(cat $fils_name | grep PoolPreparedStatements | grep true)
-#	t=$(cat $fils_name | grep TestOnBorrow | grep false)
-	if [ ! -z "$p" ] || [ ! -z "$t" ];then
+	t=$(cat $fils_name | grep TestOnBorrow | grep true)
+	tw=$(cat $fils_name | grep TestWhileIdle | grep false)
+	pp=$(cat $fils_name | grep TimeBetweenEvictionRunsMillis | grep 1000 | wc -l)
+	pp1=$(cat $fils_name | grep TimeBetweenEvictionRunsMillis | wc -l)
+	np=$(cat $fils_name | grep NumTestsPerEvictionRun | grep 5 | wc -l)
+	np1=$(cat $fils_name | grep NumTestsPerEvictionRun | wc -l)
+	me=$(cat $fils_name | grep MinEvictableIdleTimeMillis | grep 600000 | wc -l)
+	me1=$(cat $fils_name | grep MinEvictableIdleTimeMillis | wc -l)	
+	if [ ! -z "$p" ] || [ ! -z "$tw" ] || [ ! -z "$t" ] || [ ! "$pp" -eq "$pp1" ] || [ ! "$np" -eq "$np1" ] || [ ! "$me" -eq "$me1" ];then
 	    echo
-#	    echo "请正确修改servicebus.xml参数PoolPreparedStatements为false和TestOnBorrow值为true"
-	    echo "请正确修改servicebus.xml参数PoolPreparedStatements"
+	    echo "请正确修改servicebus.xml参数TestOnBorrow值为false"
+	    echo "请正确修改servicebus.xml参数PoolPreparedStatements为false"
+	    echo "请正确修改servicebus.xml参数TestWhileIdle为true"
+	    echo "请正确修改servicebus.xml参数TimeBetweenEvictionRunsMillis--Value数值为1000"
+	    echo "请正确修改servicebus.xml参数NumTestsPerEvictionRun--Value数值为5"
+	    echo "请正确修改servicebus.xml参数MinEvictableIdleTimeMillis--Value数值为600000"
 	    echo 
 	    exit 1
 	fi
