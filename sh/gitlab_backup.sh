3eee66b7b00045362cdb6b03482b435f56acc05b
indemind <616122771@qq.com>
2020-08-25 14:51:41 +0800

    debug sendmail

--- sh/gitlab_backup.sh
+++ sh/gitlab_backup.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
-source 
-gitlab_backup_path=/backup
+#set -e
 
-ret = 0
+source ~/myscript/sh/mails_cm
+
+gitlab_backup_path=/backup/gitlab/backups
+gitlab_backup_path1=/var/log/samba
+
+ret=0
 
 gitlab-backup () {
-    wihich gitlab-rake && gitlab-rake gitlab:backup:create
+    which gitlab-rake && gitlab-rake gitlab:backup:create
     if ! [ $? == "$ret" ];then
-	
-}
-
-clean-old-version () {
-    num=${ls -lrt $gitlab_backup_path | awk '{print $9}' | wc -l }
-    if [ "$num" gt 2 ];then
-	ls -lrt $gitlab_backup_path | awk '{print $9}' | head -n 1 | xargs rm -rf
+	echo "backup fails" | mails_cm -i "gitlab_code_backup"
+    else
+	clean-old-version
     fi
 }
 
+clean-old-version () {
+    if ! [ -d $gitlab_backup_path1 ];then
+	echo "backup path is not exist" | mails_cm -i "gitlab_backup_path"
+	return 0
+    fi
+	
+    num=${ls -lrt $gitlab_backup_path1 | awk '{print $9}' | wc -l }
+    if [ "$num" gt 3 ];then
+	ls -lrt $gitlab_backup_path1 | awk '{print $9}' | head -n 2 | xargs rm -f
+    fi
+}
+
+gitlab-backup
