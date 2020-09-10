5ca65fcbb4ce9d350108bf06898128b29376bf78
indemind <616122771@qq.com>
2020-09-10 18:48:01 +0800

    fix bug

--- sh/confluence_backup.sh
+++ sh/confluence_backup.sh
@@ -2,16 +2,28 @@
 
 source /etc/profile
 
-confluence_backup () {
+confluence_sys_backup () {
     docker cp  confluence:/var/atlassian/confluence/backups /backup/confluence/
 }
 
-confluence_upload () {
-    confluence_backup &&
+confluence_sys_upload () {
+    confluence_sys_backup &&
 	(
 	    cd /backup/confluence/
-	    scp -r  backups  root@192.168.50.158:/backup/confluence && echo "backup success" | mails_cm -i "confluence data backup"
+	    scp -r  backups  root@192.168.50.158:/backup/confluence && echo "backup sys data success" | mails_cm -i "confluence sys data backup"
 	)
 }
 
-confluence_upload
+confluence_data_backup () {
+    mysqldump -uroot -p123 confluence > /backup/confluence/mysql/$(date +%Y-%m-%d-%H:%M).sql
+}
+
+confluence_data_upload () {
+    confluence_data_backup &&
+	(
+	    cd /backup/confluence/
+	    scp -r  mysql  root@192.168.50.158:/backup/confluence && echo "backup msyql data success" | mails_cm -i "confluence msyql data backup"
+	)
+}
+
+confluence_sys_upload
--- sh/jira_backup.sh
+++ sh/jira_backup.sh
@@ -2,15 +2,30 @@
 
 source /etc/profile
 
-jira_backup () {
+jira_sys_backup () {
     docker cp  jira7:/var/atlassian/jira/export /backup/jira/
 }
 
-jira_upload () {
-    jira_backup &&
+jira_sys_upload () {
+    jira_sys_backup &&
 	(
-	    cd /backup/jira/
-	    scp -r  export  root@192.168.50.158:/backup/jira && echo "backup success" | mails_cm -i "jira data backup"
+	    cd /backup/jira/	    
+	    scp -r  export  root@192.168.50.158:/backup/jira && echo "backup sys data success" | mails_cm -i "jira sys data backup"
 	) 
 }
-jira_upload
+
+jira_data_backup () {
+    docker exec -it mysql mysqldump -ujira -pjira jira7 > /backup/jira/mysql/$(date +%Y-%m-%d-%H:%M).sql
+}
+
+jira_data_upload() {
+    jira_data_backup &&
+	(
+	    cd /backup/jira/
+	    scp -r  mysql  root@192.168.50.158:/backup/jira && echo "backup mysql data success" | mails_cm -i "jira mysql data backup"
+	) 
+	
+}
+
+jira_sys_upload
+jira_data_backup
