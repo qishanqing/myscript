572294f626535e1a5b85a05a5847e83b88646ae2
JENKINS <jenkins@indemind.cn>
2021-05-10 09:12:27 +0000

    dug build.sh

--- build/build.sh
+++ build/build.sh
@@ -10,32 +10,48 @@ init_project_env(){
     SUB_PROJECT_ID=$(curl -XGET -H "Content-Type: application/json" --header "PRIVATE-TOKEN: $GIT_PRIVATE_TOKEN" "http://${GIT_HOST}:85/api/v4/projects/${CONVERT_TARGET_PROJECT}"| python -c 'import sys, json; print(json.load(sys.stdin)["id"])')
     TARGET_DIR=$WORKSPACE/${TARGET_PROJECT#*/}
     SOURCE_DIR=$WORKSPACE/${SOURCE_PROJECT#*/}
+    prepare_env
 }
 
-init_system_env(){
-    system_platform=`uname -m`
-}
 
 function prepare_env() {
+    system_platform=`uname -m`
     export COMMIT_ID_FILE=${WORKSPACE}/_last_build_id
     export COMMIT_MSG_FILE=${WORKSPACE}/_commit-msg
     export COMMIT_MSG_FILE_TMP=${WORKSPACE}/_commit-msg.tmp
 }
 
 function generate_message() {
-    echo "Push $system_platform files to $BRANCH" > $COMMIT_MSG_FILE
+    echo "push $system_platform files to $BRANCH" > $COMMIT_MSG_FILE
     echo "" >> $COMMIT_MSG_FILE
     echo "job_name:      $JOB_NAME" >>   $COMMIT_MSG_FILE
     echo "build_num:      $BUILD_URL" >>  $COMMIT_MSG_FILE
     echo "" >> $COMMIT_MSG_FILE
     
     echo "=================" > $COMMIT_MSG_FILE_TMP
-    (cd $SOURCE_DIR && (git log --decorate -1)) > ${COMMIT_MSG_FILE_TMP}
-    echo "=================" > $COMMIT_MSG_FILE_TMP
+    (cd $SOURCE_DIR && (git log -1)) >> ${COMMIT_MSG_FILE_TMP}
+    echo "=================" >> $COMMIT_MSG_FILE_TMP
     
     if [[ -f $COMMIT_MSG_FILE_TMP ]];then
 	cat $COMMIT_MSG_FILE_TMP >> $COMMIT_MSG_FILE
     fi
+}
+
+function generate_commits(){
+    pushd  $TARGET_DIR
+
+    cp -ar $SOURCE_DIR/install/* .
+    git add --all .
+    git commit -s -F $COMMIT_MSG_FILE
+    
+    local remote=$(git config --local --get branch.$BRANCH.remote)
+    local branch=$(git config --local --get branch.$BRANCH.merge)
+
+    if [[ "${system_platform}" =~ "x86_64" ]];then
+        local branch=compile_pc
+    fi
+
+    git push --no-verify $remote HEAD:$branch
 }
 
 function check_status_code(){
@@ -59,18 +75,13 @@ function target_project_fetch(){
     git clone ssh://git@${GIT_HOST}:222/${TARGET_PROJECT} -b $BRANCH
 }
 
-function to_target_project_push(){
-    pushd $TARGET_DIR
-    cp-with-dir-struct $WORKSPACE/$TARGET_DIR/ $WORKSPACE/$SOURCE_DIR/install/* && cp -ar  $WORKSPACE/$TARGET_DIR/arm64/ $WORKSPACE/I18RNavigationSubmodule
 
-}
-
+init_project_env
 source_project_fetch
-
 project_build
-
-check_status_code
-
 target_project_fetch
+generate_message
+generate_commits
+
 
 
