4a51477ccf2cb0ce5599e91a8db8596bb5c12aac
JENKINS <jenkins@indemind.cn>
2021-08-16 16:09:11 +0800

    update INDEMINDAPP function

--- build/appbuild.sh
+++ build/appbuild.sh
@@ -3,21 +3,29 @@
 set -ex
 
 init_project_env(){
-    APP_WORKSPACE=$WORKSPACE/i18rApplicationDeb/work
+    BUILD_DIR=/home/khadas/workspace	
+    APP_WORKSPACE=/home/khadas/workspace/i18rApplicationDeb/work
     WORK_DIR=$APP_WORKSPACE/home/khadas/workspace
+    VERSION_FILE=$APP_WORKSPACE/DEBIAN/control
     UI_DIR=/mnt/ftp/release/INDEMINDAPP/I18R-Client
     CONFIG_DIR=/mnt/ftp/release/INDEMINDAPP/test
     DESKTOP_DIR=$APP_WORKSPACE/home/khadas/Desktop
+    PLATFORM=`uname -m`
 }
 
 function App_project_fetch(){
+    pushd $BUILD_DIR
     git clone ssh://git@192.168.50.191:222/AroundI18RProject/i18rApplicationDeb.git &&
     (
 	mkdir -p $WORK_DIR
-	pushd $WORK_DIR
+	pushd $BUILD_DIR
 	git clone --recurse-submodules  ssh://git@192.168.50.191:222/AroundI18RProject/SmallWashingRobotSDK.git -b devel/evt3 && (
 	    pushd SmallWashingRobotSDK
 	    git submodule update --remote
+            git remote set-url origin  http://192.168.50.191:85/AroundI18RProject/SmallWashingRobotSDK.git
+	    mkdir build && cd build
+	    source ../scripts/env_debug.sh
+	    cmake .. && make -j4
 	)
 	git clone ssh://git@192.168.50.191:222/AroundI18RProject/i18rmessagehandle.git &&
 	    (
@@ -25,7 +33,7 @@ function App_project_fetch(){
 		mkdir build && cd build
 		cmake ..
 		make -j4 & make install
-		cp -av /mnt/ftp/release/INDEMINDAPP/conf bin/Release/
+		cp -av /mnt/ftp/release/INDEMINDAPP/conf ../bin/Release/
 	    )
 	cp -av $CONFIG_DIR $WORK_DIR 
 	
@@ -41,17 +49,27 @@ function App_project_fetch(){
 	mkdir -p $DESKTOP_DIR
 	pushd $DESKTOP_DIR && rm -rf *
 	cp -av $UI_DIR .
-    )
-    popd
+)
+popd
 }
 
 function App_install(){
     pushd $APP_WORKSPACE
-    chmod 755 * -R
-    dpkg -b . ~/system/INDEMINDAPP_$version.deb
-    mv ~/system/INDEMINDAPP_$version.deb /mnt/ftp/release/INDEMINDAPP/
+    mv $BUILD_DIR/SmallWashingRobotSDK $WORK_DIR
+    mv $BUILD_DIR/i18rmessagehandle $WORK_DIR
+    Version_Update
+    sudo chmod 755 * -R
+    sudo chown -R khadas.khadas .
+    dpkg -b . $BUILD_DIR/INDEMINDAPP_$version.deb
+    mv $BUILD_DIR/INDEMINDAPP_$version.deb /mnt/ftp/release/INDEMINDAPP/
+}
+
+function Version_Update(){
+   sed -i s/VERSION/"$version"/g $VERSION_FILE
+   sed -i s/PLATFORM/"$PLATFORM"/g $VERSION_FILE
 }
 
 init_project_env
 App_project_fetch
 App_install
+
