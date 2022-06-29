150bdaa47c59d4ae8419b700fc712ab8f6c61596
qishanqing <qishanqing@indemind.cn>
2022-06-29 15:37:22 +0800

    change push rules for mark

--- build/build.sh
+++ build/build.sh
@@ -60,7 +60,10 @@ function generate_commits(){
         done
     fi
 
-    if ! [ -z "$TARGET_PROJECT_FILE_PATH" ];then
+    if [[ "$CLEAN_TARGET_PROJECT" = "mark-check-tools" ]];then
+	find ${SOURCE_DIR}/install  -name "*.so*" | xargs -i mv {} ${TARGET_DIR}/$TARGET_PROJECT_FILE_PATH/
+	cp -ar $SOURCE_DIR/install/* .
+    elif ! [ -z "$TARGET_PROJECT_FILE_PATH" ];then
 	mkdir -p $TARGET_PROJECT_FILE_PATH || true
 	cp -ar $SOURCE_DIR/install/* $TARGET_PROJECT_FILE_PATH/
     else
