39e6ec09f331b7cb370fa636eacc322033e2fb9c
qishanqing <qishanqing@indemind.cn>
2022-07-07 16:42:17 +0800

    check paremter for format

--- build/appbuild.sh
+++ build/appbuild.sh
@@ -37,7 +37,27 @@ init_project_env(){
     x=`echo $SWR_VERSION | perl -npe 's,_,-,g'`
     tgz_release=INTG
     mount_ftp
- }
+    check_paremter_is_right
+}
+
+function check(){
+    local a="$1"
+    printf "%d" "$a" &>/dev/null && echo "integer" && return
+    printf "%d" "$(echo $a|sed 's/^[+-]\?0\+//')" &>/dev/null && echo "integer" && return
+    printf "%d" "$(echo $a|sed 's/\.//g')" &>/dev/null && echo "vb" && return
+    printf "%f" "$a" &>/dev/null && echo "number" && return
+    [ ${#a} -eq 1 ] && echo "char" && return
+    echo "string"
+}
+
+check_paremter_is_right(){
+    if  [[ $(check "$version") == vb ]] || [[ $(check "$version") == integer ]] ;then
+	echo "check paremter is done"
+    else
+	echo "please input version number"
+	exit 1
+    fi
+}
 
 function project_info_database(){
     cmdb_mysql "insert into indemindapp(version,swr_version,submodule_version,time,note,indemind_release,sdk_branch,build_url,node_name) values ('${version}','${SWR_VERSION}','${submodule_version}',now(),'$note','${RELEASE}','${SDK_BRANCH}','$BUILD_URL','$NODE_NAME')";
