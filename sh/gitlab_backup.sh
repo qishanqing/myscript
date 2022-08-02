92ad72724ab65566fdeead9e10f970bf80b6274c
qishanqing <qishanqing@indemind.cn>
2022-08-02 18:18:14 +0800

    fixed gitlab backups error

--- sh/gitlab_backup.sh
+++ sh/gitlab_backup.sh
@@ -2,33 +2,41 @@
 set -e
 source /etc/profile
 
-gitlab_backup_path=/backup/gitlab/backups
+gitlab_backup_path=/var/opt/gitlab/backups
+gitlab_backup_path_host=/backup/gitlab/backups
 ret=0
 
 gitlab_backup () {
-    which gitlab-rake && gitlab-rake gitlab:backup:create >& ~/tmp/logs/$(date +%Y-%m-%d-%H:%M)-gitlab-backup.log
-    if ! [ $? == "$ret" ];then
-	echo "backup fails" | mails_cm -i "gitlab_code_backup"
-    else
-	echo "backup successful" | mails_cm -i "gitlab_code_backup"
-	clean_old_version
-    fi
+    docker exec -i gitlab /bin/bash <<EOF
+    set -x
+    (
+	cd $gitlab_backup_path
+	rm -f *.tar
+    )
+    which gitlab-rake && gitlab-rake gitlab:backup:create
+    exit
+EOF
+    docker cp  gitlab:$gitlab_backup_path $gitlab_backup_path_host/../
 }
 
 clean_old_version () {
-    if ! [ -d $gitlab_backup_path ];then
-	echo "backup path is not exist" | mails_cm -i "gitlab_backup_path"
-	return 0
-    fi
-	
-    num=$(ls -lrt $gitlab_backup_path | awk '{print $9}' | wc -l )
+    num=$(ls -lrt $gitlab_backup_path_host | awk '{print $9}' | wc -l )
     if [ "$num" -gt 3 ];then
 	(
-	    cd $gitlab_backup_path
+	    cd $gitlab_backup_path_host
 	    ls -lrt | awk '{print $9}' | head -n 2 | xargs rm -f    
 	)
 	
     fi
 }
 
-gitlab_backup
+gitlab_date_upload() {
+    gitlab_backup
+    clean_old_version &&
+	(
+	    scp -r -P222 $gitlab_backup_path_host  root@192.168.50.158:/backup/gitlab/ && echo "gitlab data success" | mails_cm -i "gitlab data backup"
+	)
+}
+
+
+gitlab_date_upload
