da834bd546ecec03488f1e7aebcdd3f4c7e9a653
qishanqing <qishanqing@indemind.cn>
2022-08-19 17:13:32 +0800

    update i18r_tools.sh

--- build/i18r_tools.sh
+++ build/i18r_tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-project_path=$(cd `dirname $0`; pwd)
+set -ex
 
-source $WORKSPACE/pack.sh
+project_path=$(cd `dirname $0`; pwd)
 
 init_project_env(){
     APP_WORKSPACE=~/system/i18rApplicationDeb/work
@@ -10,18 +10,20 @@ init_project_env(){
     trigger="$WORKSPACE/pack.sh"
     SOURCE_DIR="$WORKSPACE/release/i18r_robot_check"
     PACKAGE="A311D-interface-testsuite"
-    version=$BUILD_NUMBER
+    version="1.0-${BUILD_NUMBER}"
+    app_name="a311D-interface-testsuite_${version}_arm64.deb"
     TARGET_PROJECT="~/system/product_tools_group"
 }
 
 project_update() {
     rm -rf ~/system/i18rApplicationDeb  ~/system/product_tools_group
-    git clone  ssh://git@192.168.50.191:222/AroundI18RProject/i18rApplicationDeb.git ~/system/ --depth=1 
-    git clone ssh://git@192.168.50.191:222/i18r_product_tools/product_tools_group.git -b master ~/system/ --depth=1
+    git clone  ssh://git@192.168.50.191:222/AroundI18RProject/i18rApplicationDeb.git ~/system/i18rApplicationDeb --depth=1 
+    git clone ssh://git@192.168.50.191:222/i18r_product_tools/product_tools_group.git -b master $TARGET_PROJECT --depth=1
 }
 
 prebuild() {
     bash -ex $trigger
+    mkdir -p $APP_WORKSPACE/root/
     cp -av  $SOURCE_DIR/* $APP_WORKSPACE/root/
 }
 
@@ -34,15 +36,15 @@ Version_Update(){
 App_install(){
     pushd $APP_WORKSPACE
     Version_Update
-    dpkg -b . ~/system/$PACKAGE_$version_arm64.deb
+    dpkg -b . ~/system/${app_name}
     popd
 }
 
 APP_PUSH(){
     pushd $TARGET_PROJECT
     find -name *interface-testsuite*.deb | xargs git rm
-    mv ~/system/$PACKAGE_$version_arm64.deb factory_set_tools/接口测试/
-    git commit -am "upload $PACKAGE_$version_arm64.deb"
+    mv ~/system/${app_name} factory_set_tools/接口测试/
+    git commit -am "${app_name}"
     git push
 }
 
