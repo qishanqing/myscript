fb2a5f09b79d73c9a255070b9c2f2c4aae7a9d2b
qishanqing <qishanqing@indemind.cn>
2022-09-02 14:27:55 +0800

    insert db

--- build/i18r_tools_version_update.sh
+++ build/i18r_tools_version_update.sh
@@ -4,6 +4,7 @@ set -x
 project_path=$(cd `dirname $0`; pwd)
 . $project_path/../sh/version-update.sh
 . $project_path/../sh/now.
+. $project_path/../sh/cmdb
 
 init_project_env()
 {
@@ -21,6 +22,8 @@ tgz_install()
     tgz_file_name="i18r-tools-14-${build_version}-`date +%Y-%m-%d`.tgz"
     sed -i s/"$version"/"$build_version"/g $file
     tar zcvf "$tgz_file_name" * && mv $tgz_file_name $ftp_upload_path
+    git_log=`git log -1`
+    cmdb_mysql "insert into prebuild(job_name,status,first_commit_id,git_log,tgz_name,source_project,source_branch,time,build_url,node_name) values ('$JOB_NAME','0','$GIT_COMMIT','A${git_log#*A}','$tgz_file_name','${GIT_URL##*/}','$GIT_BRANCH',now(),'$BUILD_URL','$NODE_NAME')";
 }
 
 version_push()
@@ -33,4 +36,4 @@ version_push()
 init_project_env
 increment_version $version
 tgz_install
-version_push
+#version_push
