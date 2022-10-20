b9ec1af37b5cdcde4b4b088746b10969d463b9ab
qishanqing <qishanqing@indemind.cn>
2022-10-20 12:12:42 +0800

    add rbn100app.sh

---
+++ build/rbn100app.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+project_path=$(cd `dirname $0`; pwd)
+. $project_path/../sh/cmdb
+. $project_path/../sh/version-update.sh
+
+set -ex
+
+init_project_env(){
+    if [ `whoami` = root ];then
+	ios=root
+	RELEASE_DIR=/root
+	DESKTOP_DIR=/$ios/workspace/i18rApplicationDeb/work$RELEASE_DIR/workspace
+    else
+	ios=khadas
+	RELEASE_DIR=/home/$ios
+	DESKTOP_DIR=/home/$ios/workspace/i18rApplicationDeb/work$RELEASE_DIR/workspace
+    fi
+
+    BUILD_DIR=$RELEASE_DIR/workspace
+    appname=RBN100
+    sourcename=rbn100
+    APP_WORKSPACE=$RELEASE_DIR/workspace/i18rApplicationDeb/work
+    WORK_DIR=$APP_WORKSPACE$RELEASE_DIR/workspace
+    VERSION_FILE=$APP_WORKSPACE/DEBIAN/control
+    UI_DIR=/mnt/ftp/release/$appname/client
+    TEST_DIR=/mnt/ftp/release/$appname/test
+    FTP_RELEASE_DIR=/mnt/ftp/release/$appname
+    FTP_RELEASE_SIGN_DIR=$FTP_RELEASE_DIR/sign
+    FTP_RELEASE_OTA_DIR=$FTP_RELEASE_DIR/pre_release
+    FTP_RELEASE_OTA_DIFF_DIR=$FTP_RELEASE_DIR/ota
+    function_list=/mnt/ftp/release/app_update_release
+    CONFIG_DIR=~/system/i18rconfig
+    OTA_DIR=~/system/i18rota
+    PLATFORM=`uname -m`
+    CLONE_DEPTH="--depth=1"
+    ENCRYPTION_TOOL=~/system/i18rconfig/upx_arm.out
+    tgz_release=INTG
+    mount_ftp
+    check_paremter_is_right
+}
+
+function App_project_fetch(){
+    pushd $BUILD_DIR
+    git clone ssh://git@192.168.50.191:222/AroundI18RProject/i18rApplicationDeb.git $CLONE_DEPTH &&
+    (
+	mkdir -p $WORK_DIR
+	pushd $BUILD_DIR
+	git clone ssh://git@192.168.50.191:222/rbn/rbn100/integration/solutions/solution001/integration.git $sourcename -b ${SDK_BRANCH:-master} && (
+	    pushd $sourcename
+
+	    if [[ $gitmodules == true ]];then
+		cp -ar /mnt/ftp/release/${appname}/sdk/gitmodules .gitmodules
+	    fi
+	    git submodule update --init --recursive
+	    git submodule update --remote
+	    submodule_version_check
+	    release_note
+	    project_info_database
+	)
+    )
+popd
+}
+
+function App_install(){
+    pushd $APP_WORKSPACE
+    Version_Update
+    Add_Tag
+    Release_Version_Rule
+    if [[ $RELEASE = test ]];then
+	deb_type
+	mv $BUILD_DIR/INDEMINDAPP_${appname}_* $TEST_DIR
+	cmdb_mysql "update indemindapp set status='1', deb_md5ck='$deb_md5' where build_url='$BUILD_URL';"
+    elif [[ $RELEASE = true ]];then
+	SWR_VERSION=$SWR_VERSION-SIGN
+	x=`echo $SWR_VERSION | perl -npe 's,_,-,g'`
+	deb_type
+	mv $BUILD_DIR/$deb_name $FTP_RELEASE_DIR
+	echo "$sdk_note"
+	echo "$submodule_note"
+	cmdb_mysql "update indemindapp set status='0', deb_md5ck='$deb_md5', tgz_full_md5ck='$tgz_full_md5', sdk_log='$sdk_note', submodule_log='$submodule_note' where build_url='$BUILD_URL';"
+    fi
+
+    mv $BUILD_DIR/INDEMINDAPP_* $FTP_RELEASE_DIR || true
+    popd
+}
+
+function Release_Version_Rule(){
+    pushd $WORK_DIR/$sourcename
+	if [[ $PLATFORM = aarch64 ]];then
+            find -name x64 | xargs -i rm -rf {}
+	elif [[ $PLATFORM = x86_64 ]];then
+            find -name arm64 | xargs -i rm -rf {}
+	fi
+	find -name .git | xargs -i rm -rf {}
+    popd
+}
+
+init_project_env
+clean_workspace
+App_project_fetch
+App_install
