9183d05f4fc6446f21263f050818adce20bdd0f9
qishanqing <qishanqing@indemind.cn>
2022-12-02 15:47:51 +0800

    fixed parkerapp

--- build/parckerapp.sh
+++ build/parckerapp.sh
@@ -52,7 +52,7 @@ function App_project_fetch(){
     (
 	mkdir -p $WORK_DIR
 	pushd $BUILD_DIR
-	git clone ssh://git@192.168.50.191:222/parker/source/server.git -b ${SDK_BRANCH:-master} && (
+	git clone ssh://git@192.168.50.191:222/parker/source/server.git $sourcename -b ${SDK_BRANCH:-master} && (
 	    pushd $sourcename
 
 	    config_project_update
