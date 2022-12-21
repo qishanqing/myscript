ed27610111de648c918bc906e7553052c3f2d171
qishanqing <qishanqing@indemind.cn>
2022-12-21 16:50:32 +0800

    jenkins jobs update

---
+++ build/jenkins_update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export PATH="$PATH:~/myscript/sh"
+
+function get-job-info() {
+    jc get-job $JOB_NAME > $template
+}
+
+function output-manifest.xml-from-template() {
+    get-job-info
+    c=""<hudson.plugins.timestamper.TimestamperBuildWrapper plugin=timestamper@1.20/>\n  <org.jenkinsci.plugins.builduser.BuildUser plugin=build-user-vars-plugin@1.9/>
+    sed -i "s#<hudson.*#$c#g" $template
+}
+
+function jc-update-job() {
+    template="/root/tmp/logs/$JOB_NAME.xml"
+    output-manifest.xml-from-template
+
+
+    for n in $(seq 1 10);do
+	cat $template | jc update-job $JOB_NAME >& ~/tmp/logs/output.$$ &&  break || true
+	sleep 1
+    done
+}
+
+for JOB_NAME in `jc list-jobs | grep .*sdk`;do
+    jc-update-job
+done
