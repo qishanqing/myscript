89c50bc2a58e3fe9a34bfe4ef43627794e8155e7
qishanqing <qishanqing@indemind.cn>
2023-03-09 17:16:09 +0800

    update env

--- build/env.sh
+++ build/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install openjdk-11-jdk curlftpfs mysql-client ntpdate stow -y
+apt install unzip openjdk-11-jdk curlftpfs rsync mysql-client ntpdate stow libucl-dev libglew-dev glew-utils -y
 
 mkdir -p ~/system ~/tmp/logs  /mnt/ftp
 
@@ -11,6 +11,19 @@ cat ~/.ssh/id_rsa.pub
 pushd /root/system/
 curlftpfs -o rw,allow_other,nonempty ftp://guest:guest@192.168.50.191 /mnt/ftp/
 cp /mnt/ftp/slam_lib/DLib/ . -avr
+(
+    cp /mnt/ftp/slam_ThirdParty/Pangolin.zip .
+    cd Pangolin
+    mkdir build && cd build
+    cmake ..
+    cmake --build .
+    make -j4
+    make install
+    mkdir -p /usr/local/slam
+    cd /usr/local/slam
+    scp -r  root@192.168.50.142:/usr/local/slam/Thirdparty .
+)
+
 git clone ssh://git@192.168.50.191:222/sunhao/cppreview.git
 git clone ssh://git@192.168.50.191:222/AroundI18RProject/I18RPublicBaseTypes.git
 git clone ssh://git@192.168.50.191:222/AroundI18RProject/i18rconfig.git -b dev
@@ -26,5 +39,5 @@ mkswap /swapfile
 swapon /swapfile
 
 touch ~/.gitconfig
-echo "export PATH="$PATH:~/myscript/sh" >> ~/.bashrc
-echo "192.168.50.26   master-node build" >> /etc/hosts
+echo export PATH="$PATH:~/myscript/sh" >> ~/.bashrc
+echo "192.168.50.191   master-node build" >> /etc/hosts
