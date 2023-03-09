76eef0791da4d72e6b3395ee43c03f7f1b357a7d
qishanqing <qishanqing@indemind.cn>
2023-03-09 17:15:48 +0800

    update wiki jira backup

--- sh/confluence_backup.sh
+++ sh/confluence_backup.sh
@@ -2,30 +2,32 @@
 
 source /etc/profile
 
-wiki_backup_dir="/data/backup/confluence"
+wiki_backup_dir="/data/docker/wiki/etc/backups"
+wiki_backup_mysql="wiki"
+wiki_backup_mysql_path="/data/backup/mysql"
+wiki_backup_mysql_name="wiki-$(date +%Y-%m-%d-%H:%M).sql"
 confluence_sys_backup () {
-    docker cp  confluence:/var/atlassian/confluence/backups $wiki_backup_dir
+#    docker cp  confluence:/var/atlassian/confluence/backups $wiki_backup_dir
 }
 
 confluence_sys_upload () {
-    confluence_sys_backup &&
+#    confluence_sys_backup &&
 	(
-	    cd $wiki_backup_dir 
-	    scp -r -P222  backups  root@192.168.50.158:/backup/confluence && rm -rf $wiki_backup_dir/backups
+	    cd $wiki_backup_dir
+	    scp -r -P222 `ls -t | head -1` root@192.168.50.158:/backup/confluence/backups/
 	    echo "backup sys data success" | mails_cm -i "confluence sys data backup"
 	    
 	)
 }
 
 confluence_data_backup () {
-    mysqldump -uroot -p123 confluence > $wiki_backup_dir/mysql/$(date +%Y-%m-%d-%H:%M).sql
+    mysqldump -uroot -p123 $wiki_backup_mysql  > $wiki_backup_mysql_path/$wiki_backup_mysql_name
 }
 
 confluence_data_upload () {
-    confluence_data_backup &&
 	(
-	    cd $wiki_backup_dir
-	    scp -r -P222 mysql  root@192.168.50.158:/backup/confluence && echo "backup msyql data success" | mails_cm -i "confluence msyql data backup"
+	    cd $wiki_backup_mysql_path
+	    scp -r -P222 $wiki_backup_mysql_name  root@192.168.50.158:/backup/mysql/ && echo "backup msyql data success" | mails_cm -i "confluence msyql data backup"
 	)
 }
 
--- sh/jira_backup.sh
+++ sh/jira_backup.sh
@@ -3,6 +3,8 @@
 source /etc/profile
 
 jira_backup_dir=/data/backup/jira/
+jira_backup_mysql_path="/data/backup/mysql"
+jira_backup_mysql_name="jira-$(date +%Y-%m-%d-%H:%M).sql"
 jira_sys_backup () {
     docker cp  jira7:/var/atlassian/jira/export $jira_backup_dir
 }
@@ -16,14 +18,14 @@ jira_sys_upload () {
 }
 
 jira_data_backup () {
-    docker exec -i mysql mysqldump -ujira -pjira jira7 > $jira_backup_dir/mysql/$(date +%Y-%m-%d-%H:%M).sql
+    docker exec -i mysql mysqldump -ujira -pjira jira7 > $jira_backup_mysql_path/$jira_backup_mysql_name
 }
 
 jira_data_upload() {
     jira_data_backup &&
 	(
-	    cd $jira_backup_dir
-	    scp -r -P222 mysql  root@192.168.50.158:/backup/jira && echo "backup mysql data success" | mails_cm -i "jira mysql data backup"
+	    cd $jira_backup_mysql_path
+	    scp -r -P222 $jira_backup_mysql_name  root@192.168.50.158:/backup/mysql/ && echo "backup mysql data success" | mails_cm -i "jira mysql data backup"
 	) 
 	
 }
