d1c67ae298beef43415b7e50d00a080aec01f71e
qishanqing <qishanqing@indemind.cn>
2024-10-25 11:21:34 +0800

    restore sign rules

--- build/camerapp.sh
+++ build/camerapp.sh
@@ -37,9 +37,11 @@ init_project_env(){
     min_version=`cmdb_mysql "SELECT version FROM indemindapp where status='2' and swr_version='$SWR_VERSION' and indemind_release='$RELEASE' and appname='$appname' order by id desc limit 5;" | tail -n 1`
     CLONE_DEPTH="--depth=1"
     CONFIG_REMOTE="git clone ssh://git@192.168.50.191:222/AroundI18RProject/i18rconfig $CONFIG_DIR -b camera_module  $CLONE_DEPTH"
-    ENCRYPTION_TOOL="virboxprotector_con"
-    ENCRYPTION_DEAO_TOOL="dsprotector_con"
-    ENCRYPTION_TOOL_CONFIG="$CONFIG_DIR/indemind123.ssp"
+#    ENCRYPTION_TOOL="virboxprotector_con"
+#    ENCRYPTION_DEAO_TOOL="dsprotector_con"
+    #    ENCRYPTION_TOOL_CONFIG="$CONFIG_DIR/indemind123.ssp"
+    ENCRYPTION_TOOL=$CONFIG_DIR/encrypt
+    ENCRYPTION_AES_TOOL=$CONFIG_DIR/encrypt_aes
     tgz_release=INTG
     trash_dir=/mnt/ftp/Trash
     is-trigger-job
@@ -72,7 +74,7 @@ function App_install(){
     Version_Update
     Add_Tag
     Release_Version_Rule_all
-    is-sign-task-x86
+    is-sign-task
     if [[ $RELEASE = test ]];then
 	gz_type
 	mv $BUILD_DIR/INDEMINDAPP_${appname}_* $TEST_DIR ||
